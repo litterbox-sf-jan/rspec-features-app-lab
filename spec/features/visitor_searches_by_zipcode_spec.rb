7973683e7bab299b8cdf83451f6cd8130a9b8263
Alex Notov <alex@zealoushacker.com>
2015-03-03 19:59:04 -0800

    Failing spec for raining weather and wireframes

---
+++ spec/features/visitor_searches_by_zipcode_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'Visitor searches by zipcode' do
+  scenario 'raining' do
+    # setup
+    visit root_path
+
+    # exercise
+    fill_in "What's your zipcode?", with: 94702
+    click_button "Is it going to rain?"
+   
+    # verify
+    expect(page).to have_content('Looks like rain!')
+    expect(page).to have_content('Bring your umbrella')
+  end
+
+  scenario 'sunny' do
+  end
+end
